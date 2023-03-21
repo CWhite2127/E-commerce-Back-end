9423713f397030d99cf5f4164aa1f9fc93f4df3f
CWhite2127 <cwhitewesterly2@gmail.com>
2023-03-20 19:36:11 -0600

    adds finished code to e-commerce project

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,10 +7,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }, { model: ProductTag } ],
-    })
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tagged_products' } ],
+    })  
     res.status(200).json(productData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
   // find all products
@@ -48,7 +49,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
